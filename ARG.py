fixed normalization bugs

ARG.py
@@ -40,7 +40,7 @@ class ARG(nx.Graph):
         v = np.zeros(shape=(num_nodes, attribute_dim))
         for i in range(num_nodes):
             v[i] = self.nodes[i][attribute_name]
-        return v.reshape([num_nodes,-1])
+        return v
 
 if __name__ == '__main__':
     g = ARG(M={2:3},V={1})

graph_matching.py
@@ -32,7 +32,7 @@ class GraphMatching(object):
             self.ARG1 = ARG1
             self.ARG2 = ARG2
 
-        C_n, C_e = self.pre_compute_compatibility(alpha=1, stochastic=0)
+        C_n, C_e = self.pre_compute_compatibility(alpha=1, stochastic=0, node_binary=False)
         self.C_n = C_n
         self.C_e = C_e
 
@@ -159,7 +159,7 @@ class GraphMatching(object):
 
                         # print(C_e[(key_a, key_b, key_i, key_j)] , m_Head[key_a, key_i])
                 # Node attribute
-                Q = Q# + self.C_n
+                Q = Q + self.C_n
                 # Update m_Head
                 m_Head = np.exp(beta * Q)
                 m_Head[-1, -1] = 0
@@ -173,17 +173,20 @@ class GraphMatching(object):
 
                     # Begin alternative normalization.
                     # Do not consider the row or column of slacks
-                    # by column
-                    m_Head = normalize(m_Head, norm='l1', axis=0)
-                    # By row
-                    m_Head = normalize(m_Head, norm='l1', axis=1)
+                    # by row, avoid normalize with m_Head[-1, -1]
+                    m_Head_tmp = np.zeros(shape=m_Head.shape)
+                    m_Head_tmp[:-1] += normalize(m_Head[:-1], norm='l1', axis=1)
+                    # By column
+                    m_Head_tmp[:, :-1] += normalize(m_Head[:, :-1], norm='l1', axis=0)
 
+                    m_Head_tmp[:-1, :-1] /= 2
+                    m_Head = m_Head_tmp
                     # print(sum(m_Head))
                     # update converge_C
                     converge_C = sum(sum(abs(m_Head - old_C))) < self.e_C
 
                 # update converge_B
-                converge_B = abs(sum(sum(m_Head[:A, :I] - old_B[:A, :I]))) < self.e_B
+                converge_B = sum(sum(abs(m_Head[:A, :I] - old_B[:A, :I]))) < self.e_B
                 # print(converge_B, abs(sum(sum(m_Head[:A,:I]-old_B[:A,:I]))))
             # update beta
             beta *= self.beta_r
@@ -229,6 +232,7 @@ class GraphMatching(object):
 
         dim = len(atr1)
         score = np.exp(-((atr1 - atr2) ** 2).sum() / 2) / (np.sqrt(2 * np.pi) ** dim)
+        #e^((sum((a-b)^2) / 2) / sqrt(2*pi)^dim)
         # score = atr1 * atr2
         return score
 

visualize.py
@@ -9,20 +9,22 @@ import math
 from graph_matching import GraphMatching
 
 
-def generate_random_3Dgraph(n_nodes, radius, rotvect, noise, p, seed=None):
+def generate_random_3Dgraph(n_nodes, rotvect, noise, connection_rate, seed=None):
     if seed is not None:
         random.seed(seed)
 
     r = R.from_rotvec(rotvect)#[0, 0, np.pi / 2])
     # np.matmul(r.as_matrix(), np.array([1, 1, 1]))
     # Generate a dict of positions
+    G1 = nx.Graph()
     pos1 = {i: np.random.rand(3) for i in range(n_nodes)}
-    G1 = nx.random_geometric_graph(n_nodes, radius, pos=pos1, p=p, seed=seed)
+    for i in range(n_nodes):
+        G1.add_node(i, pos=pos1[i])
     for u in range(n_nodes):
         for v in range(n_nodes):
-            if u != v:
-                d1, d2, d3 = G1.nodes[u]['pos'] - G1.nodes[v]['pos']
-                G1.add_edge(u, v, eattr=math.sqrt(pow(d1, 2) + pow(d2, 2) + pow(d3, 2)))
+            if u != v and random.random() <= connection_rate:
+                dist= G1.nodes[u]['pos'] - G1.nodes[v]['pos']
+                G1.add_edge(u, v, eattr=np.linalg.norm(np.array(dist)))
 
     G2 = nx.Graph()
     n_nodes_r = (1 + noise) * n_nodes
@@ -90,8 +92,8 @@ if __name__ == '__main__':
     np.set_printoptions(precision=3)
     np.set_printoptions(suppress=True)
 
-    G1, G2, idx1, idx2 = generate_random_3Dgraph(n_nodes=8, radius=0.4, rotvect=np.array([0,0,np.pi/4]),
-                                                 noise=0, p=10, seed=1)
+    G1, G2, idx1, idx2 = generate_random_3Dgraph(n_nodes=8, rotvect=np.array([0,0,np.pi/4]),
+                                                 noise=0, connection_rate=0, seed=1)
     G1 = ARG(incoming_graph_data=G1)
     G2 = ARG(incoming_graph_data=G2)
     algorithm = GraphMatching(size=0, weight_range=0, connected_rate=0, noise_rate=0,
@@ -106,3 +108,4 @@ if __name__ == '__main__':
     network_plot_3D(fig, 121, G1, angle=10, save_folder=None)
     network_plot_3D(fig, 122, G2, angle=10, save_folder=None)
     plt.show()
+    print(1)